0dd72500b4c59a154f15429f0e4c32206b15f53e
Hung Quach <quachio@icloud.com>
2016-07-10 21:21:23 -0700

    modified index.js to print another message in red

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var colors = require('colors');
 
 exports.printMsg = function() {
   console.log('This is a message from the demo package'.green);
+  console.log('This is a message print in red'.red);
 }
